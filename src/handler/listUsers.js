8efd2feb543d1057526057a44c8d375806084741
Stephanie <stephanie.gera.amil@gmail.com>
2023-01-06 11:09:35 -0300

    delete unnecesary import of uuid

--- src/handler/listUsers.js
+++ src/handler/listUsers.js
@@ -1,6 +1,5 @@
 'use strict'
 const AWS=require ('aws-sdk');
-const uuid= require ('uuid');
 
 const dynamoDB= new AWS.DynamoDB.DocumentClient();
 module.exports.listUsers = (event, context, callback)=>{
