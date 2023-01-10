a4b3edba269956771aac761edc4f49484a1baa1a
Stephanie <stephanie.gera.amil@gmail.com>
2023-01-10 14:26:48 -0300

    adding web sockets to the app

--- handler.js
+++ handler.js
@@ -1,18 +1,124 @@
-'use strict';
+'use strict'
+const AWS=require ('aws-sdk');
+const dynamoDB= new AWS.DynamoDB.DocumentClient();
 
-module.exports.hello = async (event) => {
+module.exports.onConnect = async (event) => {
+  const connectionId = event.requestContext.connectionId;
+  const putParams = {
+    TableName: "web-socket-connections",
+    Item: {
+      connectionId: connectionId,
+    },
+  };
+  try {
+    await dynamoDB.put(putParams).promise();
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error),
+    };
+  }
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
   };
+};
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.onDisconnect = async (event) => {
+  const connectionId = event.requestContext.connectionId;
+  const delParams = {
+    TableName: "web-socket-connections",
+    Key: {
+      connectionId: connectionId,
+    },
+  };
+  try {
+    await dynamoDB.delete(delParams).promise();
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error),
+    };
+  }
+  return {
+    statusCode: 200,
+  };
+};
+module.exports.onBroadcast = async (event) => {
+  let connectionData;
+  try {
+    connectionData = await dynamoDB
+      .scan({
+        TableName: "web-socket-connections",
+        ProjectionExpression: "connectionId",
+      })
+      .promise();
+  } catch (e) {
+    return { statusCode: 500, body: e.stack };
+  }
+
+  const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+    // apiVersion: "2018-11-29",
+    endpoint:
+      event.requestContext.domainName + "/" + event.requestContext.stage,
+  });
+
+  const postData = JSON.parse(event.body).data;
+
+  const postCalls = connectionData.Items.map(async ({ connectionId }) => {
+    try {
+      await apigwManagementApi
+        .postToConnection({ ConnectionId: connectionId, Data: postData })
+        .promise();
+    } catch (e) {
+      if (e.statusCode === 410) {
+        console.log(`Found stale connection, deleting ${connectionId}`);
+        await ddb
+          .delete({
+            TableName: "web-socket-connections",
+            Key: { connectionId },
+          })
+          .promise();
+      } else {
+        throw e;
+      }
+    }
+  });
+
+  try {
+    await Promise.all(postCalls);
+  } catch (e) {
+    return { statusCode: 500, body: e.stack };
+  }
+
+  return { statusCode: 200, body: "Data sent." };
+};
+
+
+module.exports.onSend = async (event) => {
+  
+  const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+    endpoint:
+      event.requestContext.domainName + "/" + event.requestContext.stage,
+  });
+
+  const postData = JSON.parse(event.body).data;
+  const postDestination = JSON.parse(event.body).destination;
+  try {
+    await apigwManagementApi
+      .postToConnection({ ConnectionId: postDestination, Data: postData })
+      .promise();
+  } catch (e) {
+    if (e.statusCode === 410) {
+      console.log(`Found stale connection, deleting ${postDestination}`);
+      await ddb
+        .delete({
+          TableName: "web-socket-connections",
+          Key: { postDestination },
+        })
+        .promise();
+    } else {
+      throw e;
+    }
+  }
+  return { statusCode: 200, body: "Data sent." };
 };
