77bfc556756a72cb3e2bb09aa9196f22b6899d32
Stephanie <stephanie.gera.amil@gmail.com>
2023-01-23 17:20:53 -0300

    updating the get user lambda function tu use mongodb instead of dynamo

--- src/handler/getUser.js
+++ src/handler/getUser.js
@@ -1,30 +1,44 @@
-'use strict'
-const AWS=require ('aws-sdk');
+"use strict";
+const AWS = require("aws-sdk");
+const uuid = require("uuid");
+const MongoClient = require("mongodb").MongoClient;
 
-const dynamoDB= new AWS.DynamoDB.DocumentClient();
-module.exports.getUser = (event, context, callback)=>{
- 
-    const params={
-        TableName: 'users',
-        Key: {
-            id: event.pathParameters.id
-        }
+module.exports.getUser = async (event, context, callback) => {
+  const params = {
+    TableName: "users",
+    Key: {
+      id: event.pathParameters.id,
+    },
+  };
+  const client = await new MongoClient(
+    process.env.MONGO_DB_ATLAS_CONECTION_STRING,
+    {
+      useNewUrlParser: true,
     }
-    dynamoDB.get(params,(error, data)=>{
-      
-        if(error){
-            console.log(error);
-            callback(new Error(error));
-            return;
-        }
-        const response= data.Item?{
-            statusCode:200,
-            body: JSON.stringify(data.Item)
-        }:{
-            statusCode:404,
-            body: JSON.stringify({"message" : "User not found"})
-        }
-        callback(null, response);
-    })
+  );
 
-}
+  let response;
+
+  try {
+    await client.connect();
+    const db = await client.db("fff");
+    const users = await db.collection("users");
+    const usr = await users.findOne(params.Key);
+    response = {
+      statusCode: 201,
+      body: JSON.stringify({
+        message: usr,
+      }),
+    };
+  } catch (e) {
+    console.warn(e);
+    response = {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: e,
+      }),
+    };
+  } finally {
+    return response;
+  }
+};
