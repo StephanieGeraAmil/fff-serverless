6897582528aa645fd9c66740fa6efaf94e1eca62
Stephanie <stephanie.gera.amil@gmail.com>
2023-01-23 16:07:22 -0300

    using mongodb atlas instead of dynamo

--- src/handler/createUser.js
+++ src/handler/createUser.js
@@ -1,40 +1,55 @@
-'use strict'
-const AWS=require ('aws-sdk');
-const uuid= require ('uuid');
+"use strict";
+const AWS = require("aws-sdk");
+const uuid = require("uuid");
+const MongoClient = require("mongodb").MongoClient;
 
-const dynamoDB= new AWS.DynamoDB.DocumentClient();
-module.exports.createUser = (event, context, callback)=>{
-    const  now= new Date().toISOString();
-    const data= JSON.parse(event.body);
-    if ( typeof data.email != 'string'){
-        console.error ('User must have an email of type string');
-       
-        const response ={
-            statusCode: 400,
-            body: JSON.stringify({"message":"User must have an email of type string"})
-        }
-    }
-    const params={
-        TableName: 'users',
-        Item: {
-            id: uuid.v4(),
-            email: data.email,
-            createdAt: now,
-            updatedAt: now
-        }
-    }
-    dynamoDB.put(params,(error, data)=>{
-       
-        if(error){
-            console.log(error);
-            callback(new Error(error));
-            return;
-        }
-        const response={
-            statusCode:201,
-            body: JSON.stringify(data.Items)
-        }
-        callback(null, response);
-    })
+module.exports.createUser = async (event, context, callback) => {
+  const now = new Date().toISOString();
+  const data = JSON.parse(event.body);
+  if (typeof data.email != "string") {
+    const response = {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "User must have an email of type string",
+      }),
+    };
+  }
+  const params = {
+    TableName: "users",
+    Item: {
+      id: uuid.v4(),
+      email: data.email,
+      createdAt: now,
+      updatedAt: now,
+    },
+  };
+  const client = await new MongoClient(process.env.MONGO_DB_ATLAS_CONECTION_STRING, {
+    useNewUrlParser: true,
+  });
 
-}
+
+  let response;
+
+  try {
+    await client.connect();
+    const db = await client.db("fff");
+    const users = await db.collection("users");
+    const usr = await users.insertOne(params.Item);
+    response = {
+      statusCode: 201,
+      body: JSON.stringify({
+        message: usr,
+      }),
+    };
+  } catch (e) {
+    console.warn(e);
+    response = {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: e,
+      }),
+    };
+  } finally {
+    return response;
+  }
+};
