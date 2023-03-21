72cc1490511630d34096652015f4d15fe49bd033
Stephanie <stephanie.gera.amil@gmail.com>
2023-03-21 17:21:59 -0300

    delete events

--- src/handler/deleteEvent.js
+++ src/handler/deleteEvent.js
@@ -1,53 +1,51 @@
 "use strict";
 const AWS = require("aws-sdk");
-const uuid = require("uuid");
-const MongoClient = require("mongodb").MongoClient;
+const getClient = require("../mongo_client.js");
 
-module.exports.deleteEvent = async (event, context, callback) => {
-  const params = {
-    TableName: "events",
-    Key: {
-      id: event.pathParameters.id,
-    },
-  };
-  const client = await new MongoClient(
-    process.env.MONGO_DB_ATLAS_CONECTION_STRING,
-    {
-      useNewUrlParser: true,
-    }
-  );
-
-  let response;
-
+module.exports.deleteEvent = async (event) => {
   try {
-    await client.connect();
+
+    const client = await getClient.getClient();
+    const now = new Date().toISOString();
+    const data = JSON.parse(event.body);
+    const key = {id:data.event};
     const db = await client.db("fff");
-    const Events = await db.collection("events");
-    const usr = await Events.deleteOne(params.Key);
-    if (usr !== null) {
-      response = {
-        statusCode: 202,
-        body: JSON.stringify({
-          message: usr,
-        }),
-      };
-    } else {
-      response = {
-        statusCode: 500,
-        body: JSON.stringify({
-          message: "Event not found",
-        }),
-      };
-    }
-  } catch (e) {
-    console.warn(e);
-    response = {
-      statusCode: 400,
-      body: JSON.stringify({
-        message: e,
-      }),
+    const eventsTable = await db.collection("events");
+    const result = await eventsTable.deleteOne(key);
+    if (result === null) return;
+
+    const liveConnectionsTable = await db.collection("web-socket-connections");
+
+    const liveConnections = await liveConnectionsTable.find().toArray();
+
+    const endpoint =
+      event.requestContext.domainName + "/" + event.requestContext.stage;
+
+    const apigatewaymanagementapi = new AWS.ApiGatewayManagementApi({
+      endpoint: endpoint,
+    });
+
+    const post = async (connectionId, message) => {
+      return await apigatewaymanagementapi
+        .postToConnection({
+          ConnectionId: connectionId,
+          Data: Buffer.from(JSON.stringify(message)),
+        })
+        .promise();
     };
-  } finally {
-    return response;
+
+    const postCalls = liveConnections.map(async (connection) => {
+      const newEventMessage = JSON.stringify({
+        action: "deletedEvent",
+        data: data.event,
+      });
+      return await post(connection.connectionId, newEventMessage);
+    });
+
+    await Promise.all(postCalls);
+
+    return;
+  } catch (e) {
+    return { statusCode: 500, body: e.stack };
   }
 };
